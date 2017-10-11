56ba18bac51c4310130b3fbc7a43b2b36caa453e
Francis Breidenbach <fbreidenbach@gmail.com>
2017-10-11 00:07:37 -0700

    Add Platform retrieval from Realm to protocol.

--- GamerDB/Services/Platform/PlatformServiceType.swift
+++ GamerDB/Services/Platform/PlatformServiceType.swift
@@ -14,7 +14,16 @@ import RealmSwift
 
 protocol PlatformServiceType {
   
+  /// Fetch latest generation platforms and save to local Realm database.
+  ///
+  /// - Returns: Discardable observable array of Platform objects.
   @discardableResult
   func fetchLatestGenPlatforms() -> Observable<[Platform]>
+  
+  
+  /// Retrieve all platforms saved to local Realm database.
+  ///
+  /// - Returns: Observable Realm results of type Platform.
+  func platforms() -> Observable<Results<Platform>>
   
 }
